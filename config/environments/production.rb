4e74b533f8ad97dc29fbef93369d896f5f717ae2
mindatasleep <mindatasleep@gmail.com>
2019-12-18 09:44:33 -0700

    Create home page for SmoothPeaks.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,11 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  # Custom...
+  config.assets.digest = true
+  config.assets.compress = true
+  config.serve_static_assets = false
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
