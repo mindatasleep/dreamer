32632d6a0cd4da3ac6d05a40cf91567abab724d9
mindatasleep <mindatasleep@gmail.com>
2019-12-18 14:23:18 -0700

    Create home page for SmoothPeaks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   root to: 'pages#home'
+  # get "/oakley", to: static("oakley.html")
+  get "/oakley" => "pages#oakley"
   # root to: redirect('/posts')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
