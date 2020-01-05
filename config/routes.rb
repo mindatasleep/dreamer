0e0eae796841c68ae9d368e77c46551f4be04ea3
mindatasleep <mindatasleep@gmail.com>
2020-01-05 07:07:29 -0700

    Touch up home page header design

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   root to: 'pages#home'
-  # get "/oakley", to: static("oakley.html")
-  get "/oakley" => "pages#oakley"
-  # root to: redirect('/posts')
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
